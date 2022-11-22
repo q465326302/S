9381a2d58308abb278d737884654fb2bbb6c498f
465326302 <1333555>
2022-11-22 19:40:47 +0800

    do while

--- exercise/InsertionSort.sol
+++ exercise/InsertionSort.sol
@@ -9,10 +9,28 @@ contract InsertionSort{
         }
     }
     function forLoopTest() public pure returns(uint256){
-        uint sum = 0;
-        for(uint i = 0;i <3;i++){
+        uint sum = 2;
+        for(uint i = 0;i <8;i++){
             sum += i;
         }
+        return(sum);
+    }
+    function whileTest() public pure returns(uint256) {
+        uint sum = 0;
+        uint i = 0;
+        while(i < 10){
+            sum += i ;
+            i++;
+        }
+        return(sum);
+    }
+    function doWileTest() public pure returns(uint256) {
+        uint sum = 0;
+        uint i = 0;
+        do{
+            sum += 1;
+            i++;
+        }while (i < 10);
         return(sum);
     }
 }
