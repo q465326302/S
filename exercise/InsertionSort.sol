b1b339a13c5d49fcdea00bf125e835126e1762bb
465326302 <1333555>
2022-11-22 19:08:54 +0800

    shangchuan

--- exercise/InsertionSort.sol
+++ exercise/InsertionSort.sol
@@ -1,5 +1,18 @@
 pragma solidity ^0.8.4;
 contract InsertionSort{
     //if-else;
-    function ifElseTest(uint256 _number) public pure 
+    function ifElseTest(uint256 _number) public pure returns(bool) {
+        if(_number ==0) {
+            return(true);
+        }else{
+            return(false);
+        }
+    }
+    function forLoopTest() public pure returns(uint256){
+        uint sum = 0;
+        for(uint i = 0;i <10;i++){
+            sum += i;
+        }
+        return(sum);
+    }
 }
