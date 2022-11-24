28031d8069e06960a56a000ba91b15d922db32b7
465326302 <465326302@qq.com>
2022-11-24 20:45:05 +0800

    error

--- exercise/Error.sol
+++ exercise/Error.sol
@@ -1,19 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 error TransferNotOwner();
-contract Errors{
-    mapping(uint256 =>address) private _owner;
-    function TransferNotOwner1(uint256 tokenId,address newOwner) public {
-        if (_owners[tokenId] != msg.sender){
-            revert  TransferNotOwner();
+contract Errors {
+    mapping(uint256 => address) private _owners;
+    function transferOwner1(uint256 tokenId,address newOwner) public {
+        if (_owners[tokenId] != msg.sender) {
+            revert TransferNotOwner();
         }
         _owners[tokenId] = newOwner;
     }
-    function TransferNotOwner2(uint256 to , address newOwner) public{
-        require(_owners[tokenId] == msg.sender,"Transfer Not Owner");
+    function transferOwner2(uint256 tokenId , address newOwner) public {
+        require(_owners[tokenId] == msg.sender);
         _owners[tokenId] = newOwner;
     }
-    function TransferNotOwner3(uint256 tokenId,address newOwner) public {
+    function transferOwner3(uint256 tokenId,address newOwner) public {
         assert(_owners[tokenId] == msg.sender);
         _owners[tokenId] = newOwner;
     }
