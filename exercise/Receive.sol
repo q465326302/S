5ee9860164f2b2c841b6db66a0fceb1775d4834e
465326302 <465326302@qq.com>
2022-11-25 21:01:05 +0800

    receive

--- exercise/Receive.sol
+++ exercise/Receive.sol
@@ -1,12 +1,9 @@
 //SPDX-License-Identifier:MIF
 pragma solidity ^0.8.4;
 contract Receive{
-    event receivedCalled(address Sender ,uint Value);
-    event fallbackCalled(address Sender,uint Value,bytes Dota);
+    event Received(address Sender,uint Value,string message);
     receive()external payable {
-        emit receivedCalled(msg.sender,msg.value);
+        emit Received(msg.sender,msg.value,'receive');
     }
-    fallback() external payable{
-        emit fallbackCalled(msg.sender,msg.value, msg.data);
-    }
+
 }
