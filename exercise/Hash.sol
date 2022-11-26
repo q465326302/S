216537697a9cd3f94cd6e469ddc7a6f6739d29a3
465326302 <465326302@qq.com>
2022-11-26 16:51:27 +0800

    hash

--- exercise/Hash.sol
+++ exercise/Hash.sol
@@ -2,4 +2,24 @@
 pragma solidity ^0.8.4;
 contract Hash{
     bytes32 _msg = keccak256(abi.encodePacked("0xAA"));
+    function hash(
+        uint _num,
+        string memory _string,
+        address _addr
+    ) public pure returns(bytes32){
+        return keccak256(abi.encodePacked(_num, _string, _addr));
+    }
+
+    function weak(//弱抗碰撞性
+        string memory string1
+    ) public view returns(bool){
+        return keccak256(abi.encodePacked(string1))  == _msg;
+    }
+
+    function strong(//强抗碰撞性
+        string memory string1,
+        string memory string2
+    ) public pure returns (bool){
+        return keccak256(abi.encodePacked(string1)) == keccak256(abi.encodePacked(string2));
+    }
 }
