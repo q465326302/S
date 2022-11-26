5206929c51339c18ea6f5bfa1f7f50e76073bf5c
465326302 <465326302@qq.com>
2022-11-26 14:44:46 +0800

    selector

--- exercise/Selector.sol
+++ exercise/Selector.sol
@@ -8,4 +8,8 @@ contract Seletor{
     function mintSelector() external pure returns(bytes4 mSelector){
         return bytes4(keccak256("mint(address)"));
     }
+    function callWithSignature() external returns(bool,bytes memory){
+        (bool success, bytes memory data) = address(this).call(abi.encodeWithSelector(0x6a627842,"0x2c44b726ADF1963cA47Af88B284C06f30380fC78"));
+        return(success,data);
+        }
 }
