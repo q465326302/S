12778441c9692c806d93055983ba140b124ec0db
465326302 <465326302@qq.com>
2022-11-28 21:16:55 +0800

    call

--- exercise/call.sol
+++ exercise/call.sol
@@ -50,3 +50,11 @@ contract Call{
         emit Response(success, data);
     }
 }
+pragma solidity >=0.6.2 <0.9.0;
+contract Fund {
+    mapping(address => uint) shares;
+    function withdraw() public {
+        if (payable(msg.sender).call.value(shares[msg.sender])())
+            shares[msg.sender] = 0;
+    }
+}
