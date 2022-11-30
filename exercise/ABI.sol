eef8aa25d8a4aac95d125f467c4f593ffbe90e09
465326302 <465326302@qq.com>
2022-11-30 21:01:42 +0800

    ABI

--- exercise/ABI.sol
+++ exercise/ABI.sol
@@ -4,10 +4,20 @@ contract ABIEncode{
     uint x = 10;
     address addr = 0x7A58c0Be72BE218B41C608b7Fe7C5bB630736C71;
     string name = "0xAA";
-    uint[2] arrary = [5,6];
+    uint[2] array = [5,6];
 
     function encode() public view returns(bytes memory result){
-        result = abi.encode(x,addr,name,arrary);
+        result = abi.encode(x,addr,name,array);
 
     }
+    function encodePacked() public view returns(bytes memory result) {
+        result = abi.encodePacked(x, addr, name, array);
+    }
+    function encodeWithSignature() public view returns(bytes memory result){
+        result = abi.encodeWithSignature("foo(uint256,address,string,uint256[2]", x, addr, name, array);
+    }
+    function encodeWithSelector()public view returns(bytes memory result) {
+        result = abi.encodeWithSelector(bytes4(keccak256("foo(uint256,address, string,uint[2]"))), x, addr, name, array);
+    }
+
 }
