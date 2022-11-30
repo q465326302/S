5fc4aa86909e66355a4421b6fc870c2000a48900
465326302 <465326302@qq.com>
2022-11-30 15:49:36 +0800

    合约里创合约

---
+++ exercise/create2.sol
@@ -0,0 +1,17 @@
+//SPDX-Licen-Identifier: MIT
+pragma solidity ^0.8.4;
+contract Pair {
+    address public factory;
+    address public token0;
+    address public token1;
+
+    constructor() payable {
+        factory = msg.sender;
+    }
+
+    function initialize(address _token0,address _token1) external {
+        require(msg.sender == factory,'UniswapV2:FORBIDDEN');
+        token0 = _token0;
+        token1 = _token1;
+    }
+}
