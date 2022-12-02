345d867b0a3e670c02a6a254f40e4bb10800d6ab
465326302 <465326302@qq.com>
2022-12-02 19:55:57 +0800

    空投

--- 应用exercise/Airdrop.sol
+++ 应用exercise/Airdrop.sol
@@ -9,15 +9,39 @@ contract Airdrop {
     function multiTransferToken(
         address _token,
         address[] calldata _addresses,
-        uint256[] calldata _amount
+        uint256[] calldata _amounts
     ) external {
-        require(_addresses.Length == _amount.Length,"Lengths of Addresses and Amounts NOT EQUAL");
-        IERC20 tokenContract = IERC20(_token);
+        require(_addresses.length == _amounts.length,"Lengths of Addresses and Amounts NOT EQUAL");
+        IERC20 token = IERC20(_token);
         uint _amountSum = getSum(_amounts);
-        require(token.allowance(msg.sender, address(this)) > _amountSum, "Need Approve ERC20 token");
+        require (token.allowance(msg.sender, address(this)) > _amountSum, "Need Approve ERC20 token");
 
-        for (uint256 i;i < _addresses,Length; i++){
-            
+        for (uint256 i; i < _addresses.length; i++) {
+            token.transferFrom(msg.sender, _addresses[i], _amounts[i]);
         }
     }
+
+    function multiTransrETH(
+        address payable[] calldata _addresses,
+        uint256[] calldata _amounts
+    ) public payable {
+        require(_addresses.length == _amounts.length,"Lengths of Amounts NOT EQUAL");
+        uint _amountSum = getSum(_amounts);
+        require(msg.value == _amountSum,"Trans amount error");
+        for (uint256 i = 0; i < _addresses.length; i++){
+            _addresses[i].transfer(_amounts[i]);
+        }
+    }
+    
+    function getSum(uint256[] calldata _arr) public pure returns(uint sum)
+    {
+        for(uint i = 0; i <_arr.length; i++)
+            sum = sum +_arr[i];
+    }
+}
+contract ERC20 is IERC20 {
+    mapping(address => uint256) public override balanceOf;
+    mapping(address => mapping(address => uint)) public override allowance;
+    uint256 public override totalSupply;
+    constructor(string memory name_, string memory)
 }
