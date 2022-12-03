7e84264ff0c5499b12e56d275c50240bfd154559
465326302 <465326302@qq.com>
2022-12-03 14:32:21 +0800

    ierc721

---
+++ 应用exercise/IERC721.sol
@@ -0,0 +1,42 @@
+//SPDX-Licen-Identifier:MIT
+
+pragma solidity ^0.8.0;
+
+import "./IERC165.sol";
+
+interface IERC721 is IERC165 {
+    event Transfer(address indexed form, address indexed to, uint256 indexed tokenId);
+    event Approval(address indexed owner,address indexed approved,uint256 indexed token);
+    event ApprovelForAlll(address indexed owner,address indexed operator,bool approved);
+
+    function balanceOf(address owner) external view returns(uint256 balanceOf);
+    
+    function ownerOf(uint256 tokenId) external view returns (address owner);
+
+    function safeTransferForm(
+        address form,
+        address to,
+        uint256 token,
+        bytes calldata data
+    ) external;
+
+    function safeTransferForm(
+        address form,
+        address to,
+        uint256 tokenId
+    ) external;
+
+    function transferFrom(
+        address form,
+        address to,
+        uint256 tokenId
+    ) external;
+
+    function approve(address to, uint256 tokenId) external;
+
+    function setApprovalForAll(address operator, bool _approved) external;
+
+    function getApproved(uint256 tokenId) external view returns (address operator);
+
+    function isApprovedForAll(address owner,address operator) external view returns(bool);
+}
