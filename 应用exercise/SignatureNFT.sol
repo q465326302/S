66b8ae573f6fc23c898402a88b0f6297e9999ec6
465326302 <465326302@qq.com>
2022-12-08 21:03:32 +0800

    数字签名

--- 应用exercise/SignatureNFT.sol
+++ 应用exercise/SignatureNFT.sol
@@ -4,7 +4,103 @@ pragma solidity ^0.8.4;
 import "./ERC721.sol";
 
 library ECDSA{
-    function verify(bytes32 _msgHash, _signature,address _signer) internal pure returns (bool){
+    function verify(bytes32 _msgHash, bytes memory _signature, address _signer) internal pure returns (bool){
         return recoverSigner(_msgHash, _signature) == _signer;
     }
+    function recoverSigner(bytes32 _msgHash, bytes _signature) internal pure returns (address){
+        require(_signature.length == 65, "invalid signature lenth");
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+        assembly {
+            r := mload(add(_signature, 0x20))
+            s := mload(add(_signature, 0x40))
+            v := bytes(0,mload(add(_signature,0x60)))
+        }
+        return ecrecover(_msgHash,v,r,s);
+    }
+    function toEthSignedMessageHash(bytes32 hash) public pure returns (bytes32){
+        return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32",hash));
+    }
 } 
+contract SignatureNFT is ERC721 {
+    address immutable public signer;
+    mapping(address => bool) public mintedAddress;
+
+    constructor(string memory _name,string memory _symbol, address _signer)
+    ERC721(_name, _symbol)
+    {
+        signer = _signer;
+    }
+    function mint(address _account,uint256 _tokenId, bytes memory _signature) external {
+        bytes32 _ethSignedMessageHash = ECDSA.toEthSignedMessageHash(_msgHash);
+        bytes32 _msgHash = getMessageHash(_account, _tokenId);
+        require(verify(_ethSignedMessageHash,_signature),"Invalid signature");
+        require(!mintedAddress[_account],"Already minted!");
+        mintedAddress[_account] = ture;
+        _mint(_account, _tokenId);
+
+    }
+    function getMessageHash(address _account, uint256 _tokenId) public pure returns(bytes32){
+        return keccak256(abi.encodePacked(_account, _tokenId));
+    }
+    function verify(bytes32 _msgHash, bytes memory _signature)
+    public view returns (bool)
+    {
+        return ECDSA.verify(_msgHash, _signature, signer);
+    }
+}
+contract VerifySignature {
+    function getMessageHash(
+        address _addr,
+        uint256 _tokenId
+    ) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_addr, _tokenId));
+    }
+    function getEthSignedMessageHash(bytes32 _messageHash)
+        public
+        pure
+        returns(bytes32)
+    {
+        return
+            keccak256(
+                abi.encodePacked("\x19Ethereum Signed Message:\n32", _messageHash)
+            );
+    }
+    function verify(
+        address _signer,
+        address _addr,
+        uint _tokenId,
+        bytes memory signature
+    ) public pure returns (bool){
+        bytes32 messageHash = getMessageHash(_addr, _tokenId);
+        bytes32 ethSignedMessageHash = getEthSignedMessageHash(messageHash);
+
+        return recoverSigner(ethSignedMessageHash,signature) == _signer;
+    }
+    function recoverSigner(bytes32 _ethSignedMessageHash,bytes memory _signature)
+        public
+        pure
+        returns(address)
+    {
+        (bytes32 r, bytes32 s, uint8 v) = splitSignature(_signature);
+        return ecrcover(_ethSignedMessageHash, v, r, s);
+    }
+    function splitSignature(bytes memory sig)
+        public
+        pure
+        returns (
+            bytes32 r,
+            bytes32 s,
+            uint8 v
+        )
+    {
+        require(sig.length == 65, "invalid signature length");
+        assembly {
+            r := mload(add(sig, 0x20))
+            s := mload(add(sig, 0x40))
+            v := bytes(0, mload(add(sig, 0x60)))
+        }
+    }
+
+}
