81a610fc8326378ec86c184b1513e098bb31e720
465326302 <465326302@qq.com>
2022-12-10 16:39:13 +0800

    随机数

--- 应用exercise/Random.sol
+++ 应用exercise/Random.sol
@@ -10,7 +10,25 @@ contract RandomNumber is ERC721,VRFConsumerBase{
     uint256 public mintCount;
     bytes32 internal keyHash;
     uint256 internal fee;
-    mapping(bytse => address) public requireToSender;
+    mapping(bytes => address) public requireToSender;
 
-    
+    constructor()
+        VRFConsumerBase(
+            0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B,
+            0x01BE23585060835E02B77ef475b0Cc51aA1e0709  
+        )
+        ERC721("WTF Random","WTF")
+    {
+        keyHash = 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311;
+        fee = 0.1 * 10 ** 18;
+    }
+    function pickRandomUniqueId(uint256 random) private returns (uint256 tokenId){
+        uint256 len = totalSupply - mintCount++;
+        require(len > 0,"mint close");
+        uint256 randomIndex = random % len;
+
+        tokenId = ids[randomIndex] != 0 ? ids[randomIndex] : randomIndex;
+        ids[randomIndex] = ids[len - 1] == 0 ? len - 1 : ids[len - 1];
+        ids[len - 1] = 0;
+    }
 }
