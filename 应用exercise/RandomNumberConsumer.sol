04f7491f127e3d0f565a0256299158ce21d69e53
465326302 <465326302@qq.com>
2022-12-10 15:32:17 +0800

    链下随机数

---
+++ 应用exercise/RandomNumberConsumer.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+
+contract RandomNumberConsumer is VRFConsumerBase {
+    bytes32 internal keyHash;
+    uint256 internal fee;
+    uint256 public randomResult;
+    
+    constructor()
+        VRFConsumerBase(
+            0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B,
+            0x01BE23585060835E02B77ef475b0Cc51aA1e0709
+        )
+    {
+        keyHash = 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311;
+        fee = 0.1 * 10 **18;
+    }
+}
