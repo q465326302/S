3422e9f71633907eff9400376af67f9bb9ddb2b9
465326302 <465326302@qq.com>
2022-12-12 15:06:01 +0800

    ERC1155接受合约

---
+++ 应用exercise/IERC1155Receiver.sol
@@ -0,0 +1,21 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.0;
+import "./IERC165.sol";
+
+interface IERC1155 is IERC165 {
+    function onERC1155Received(
+        address operator,
+        address from,
+        uint256 id,
+        uint256 value,
+        bytes calldata data
+    ) external returns (bytes4);
+    //单币转账函数，接受ERC1155安全转账safetransferfrom 需实现并返回自己的函数选择器0x623a6e61
+    function onERC1155BatchReceived(
+        address operator,
+        address from,
+        uint256[] calldata ids,
+        uint256[] calldata value,
+        bytes calldata data
+    ) external returns(bytes4);
+}
