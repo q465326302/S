621d18f607f3a78ae1f22aca8a58ddada969d4ba
465326302 <465326302@qq.com>
2023-01-12 22:24:58 +0800

    第四关 攻击

--- Ethernaut/TelephoneAttack.sol
+++ Ethernaut/TelephoneAttack.sol
@@ -1,22 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Telephone {
-
-  address public owner;
-
-  constructor() {
-    owner = msg.sender;
-  }
-
-  function changeOwner(address _owner) public {
-    if (tx.origin != msg.sender) {
-      owner = _owner;
-    }
-  }
-}
-
-import "./Telephone.sol";
+import './Telephone.sol';
 
 contract TelephonePoc {
     
