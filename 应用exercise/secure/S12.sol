0f440b0e8a320935dc7aac347145288f195f2d2e
465326302 <465326302@qq.com>
2023-01-31 19:42:46 +0800

    s11 BANK

---
+++ 应用exercise/secure/S12.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+contract Bank {
+
+
+    address public owner;
+     constructor() payable {
+        owner = msg.sender;
+    }
+
+    function transfer(address payable _to, uint _amount) public {
+        require(tx.origin == owner, "Not owner");
+        (bool sent, ) = _to.call{value: _amount}("");
+        require(sent, "Failed to send Ether");
+    }
+}
