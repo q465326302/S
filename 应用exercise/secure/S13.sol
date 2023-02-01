baba0eff639434d1e2b2886eba019e73b4e5c533
465326302 <465326302@qq.com>
2023-02-01 12:47:25 +0800

    S13 银行合约

---
+++ 应用exercise/secure/S13.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+// by 0xAA
+pragma solidity ^0.8.4;
+
+contract UncheckedBank {
+    mapping (address => uint256) public balanceOf;
+    function deposit() external payable {
+        balanceOf[msg.sender] += msg.value;
+    }
+    function withdraw() external {
+        uint256 balance = balanceOf[msg.sender];
+        require(balance > 0, "Insufficient balance");
+        balanceOf[msg.sender] = 0;
+        bool success = payable(msg.sender).send(balance);
+    }
+    function getBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
+}
