7f9a76c64a2ed1e6a478bacb8b0530759a787df3
465326302 <465326302@qq.com>
2023-02-09 21:41:35 +0800

    Privacy 过关

---
+++ Ethernaut/Privacy.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Privacy {
+
+  bool public locked = true;
+  uint256 public ID = block.timestamp;
+  uint8 private flattening = 10;
+  uint8 private denomination = 255;
+  uint16 private awkwardness = uint16(block.timestamp);
+  bytes32[3] private data;
+
+  constructor(bytes32[3] memory _data) {
+    data = _data;
+  }
+  
+  function unlock(bytes16 _key) public {
+    require(_key == bytes16(data[2]));
+    locked = false;
+  }
+}
+//0x4D1bf8CCbBf9903268790b78E63014A35f1AAc99
+//0x744b4795141847ffc0b261ffd95b3641
