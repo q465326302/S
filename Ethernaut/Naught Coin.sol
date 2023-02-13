a80d2358799f9281d8ac9bbb02952be7de05b7c2
465326302 <465326302@qq.com>
2023-02-13 21:38:55 +0800

    16 结束

--- Ethernaut/Naught Coin.sol
+++ Ethernaut/Naught Coin.sol
@@ -5,9 +5,7 @@ import 'openzeppelin-contracts-08/token/ERC20/ERC20.sol';
 
  contract NaughtCoin is ERC20 {
 
-  // string public constant name = 'NaughtCoin';
-  // string public constant symbol = '0x0';
-  // uint public constant decimals = 18;
+
   uint public timeLock = block.timestamp + 10 * 365 days;
   uint256 public INITIAL_SUPPLY;
   address public player;
@@ -16,8 +14,7 @@ import 'openzeppelin-contracts-08/token/ERC20/ERC20.sol';
   ERC20('NaughtCoin', '0x0') {
     player = _player;
     INITIAL_SUPPLY = 1000000 * (10**uint256(decimals()));
-    // _totalSupply = INITIAL_SUPPLY;
-    // _balances[player] = INITIAL_SUPPLY;
+
     _mint(player, INITIAL_SUPPLY);
     emit Transfer(address(0), player, INITIAL_SUPPLY);
   }
@@ -26,7 +23,6 @@ import 'openzeppelin-contracts-08/token/ERC20/ERC20.sol';
     super.transfer(_to, _value);
   }
 
-  // Prevent the initial owner from transferring tokens until the timelock has passed
   modifier lockTokens() {
     if (msg.sender == player) {
       require(block.timestamp > timeLock);
