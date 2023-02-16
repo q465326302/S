b412f96280f6840fa25ac933babebb9eff6270ad
465326302 <465326302@qq.com>
2023-02-17 00:15:32 +0800

    MagicNum

--- Ethernaut/MagicNumber.sol
+++ Ethernaut/MagicNumber.sol
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+contract Hack {
+    constructor(IMagicNum target) {
+       
+        address addr;
+    }
 contract MagicNum {
 
   address public solver;
