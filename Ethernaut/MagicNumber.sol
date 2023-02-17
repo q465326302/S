0035b80cb8214511c3642c7494c9744bd5836d61
465326302 <465326302@qq.com>
2023-02-17 18:13:44 +0800

    Alien Codex

--- Ethernaut/MagicNumber.sol
+++ Ethernaut/MagicNumber.sol
@@ -1,11 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+interface IMagicNum {
+    function solver() external view returns (address);
+    function setSolver(address) external;
+}
+
+interface ISolver {
+    function whatIsTheMeaningOfLife() external view returns (uint256);
+}
+
 contract Hack {
     constructor(IMagicNum target) {
-        bytes memory bytecode = ;
+        bytes memory bytecode = hex"69602a60005260206000f3600052600a6016f3";
         address addr;
+        assembly {
+            addr := create(0, add(bytecode, 0x20), 0x13)
+        }
+        require(addr != address(0));
+
+        target.setSolver(addr);
     }
+}
 contract MagicNum {
 
   address public solver;
@@ -15,3 +31,4 @@ contract MagicNum {
   function setSolver(address _solver) public {
     solver = _solver;
   }
+}
