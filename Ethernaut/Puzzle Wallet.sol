2fd30780798b63eb2df9b7326b691ea0ec80bd76
465326302 <465326302@qq.com>
2023-02-25 13:30:33 +0800

    开始

--- Ethernaut/Puzzle Wallet.sol
+++ Ethernaut/Puzzle Wallet.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
 interface IWallet {
     function admin() external view returns (address);
     function proposeNewAdmin(address _newAdmin) external;
