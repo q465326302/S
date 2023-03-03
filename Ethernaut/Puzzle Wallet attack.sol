e92bc1ea949bd79e6358d3ffc4284731fe481559
465326302 <465326302@qq.com>
2023-03-03 23:00:15 +0800

    结束

---
+++ Ethernaut/Puzzle Wallet attack.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.8.0;
+
+contract Attack {
+    address payable public wallet;
+
+    constructor(address payable _wallet) {
+        wallet = _wallet;
+    }
+
+    function attack() public {
+        wallet.call{value: address(this).balance}("");
+    }
+    
+    // fallback function to receive ether
+    receive() external payable {}
+}
