1f39cee65a2820e82bd15ed213e6253b7833ff1e
465326302 <465326302@qq.com>
2023-03-07 14:55:25 +0800

    开始

--- Ethernaut/gatekeeper3 attack.sol
+++ Ethernaut/gatekeeper3 attack.sol
@@ -24,6 +24,7 @@ contract SimpleTrick {
     
   function trickyTrick() public {
     if (address(this) == msg.sender && address(this) != trick) {
+      
       target.getAllowance(password);
     }
   }
