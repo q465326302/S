efdaa081d45483f8be662527b6cee84e67d76557
465326302 <465326302@qq.com>
2023-03-08 12:53:36 +0800

    开始

--- Ethernaut/Naught Coin.sol
+++ Ethernaut/Naught Coin.sol
@@ -5,6 +5,7 @@ interface INaughtCoin {
   function player() external view returns (address);
 }
 interface IERC20 {
+  //
   function balanceOf(address account) external view returns (uint256);
   function  approve(address spender, uint256 amount) external returns (bool);
   function transferFrom(address sender,address recipient,uint256 amount)external returns (bool);
@@ -17,4 +18,4 @@ contract Hack {
         coin.transferFrom(player, address(this), bal);
     }
 }
-//0x270CFd2527416122C70E6BbF156f4acC1Ee63C09
+
