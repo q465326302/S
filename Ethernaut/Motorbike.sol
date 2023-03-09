72ebd2d113848da10bf3f9185a569d1b52b8f182
465326302 <465326302@qq.com>
2023-03-09 13:57:52 +0800

    开始

--- Ethernaut/Motorbike.sol
+++ Ethernaut/Motorbike.sol
@@ -10,9 +10,6 @@ interface IEngine {
     function upgradeToAndCall(address newImplementation, bytes memory data) external payable;
 }
 
-// Get implementation
-// await web3.eth.getStorageAt(contract.address, '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc')
-// 0x0000000000000000000000002c61fbf57208d2b3234d1383d84388a77c251bc5
 
 contract Hack {
     function pwn(IEngine target) external {
