5758b9e412f580c57ff1669f39d78c3e807bd9f6
465326302 <465326302@qq.com>
2023-03-15 10:11:01 +0800

    开始

---
+++ exercise/29 选择器.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.0;
+
+contract selector{
+    event Log(bytes data);
+    function mint(address to)external{
+        emit Log(msg.data);
+    }
+
+    function mintSelector()external pure returns(bytes4 mSelector) {
+        return bytes4(keccak256("mint(address)"));
+    }
+    function callWithSignature() external returns(bool,bytes memory){
+        (bool success,bytes memory data) = address(this).call(abi.encodeWithSelector(0x6a627842,"0x2c44b726ADF1963cA47Af88B284C06f30380fC78"));
+        return(success,data);
+    }
+}
---
+++ exercise/46 代理合约.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.0;
+
+contract Proxy{
+    // implementtation为以太坊地址
+    address public implementation;
+
+    constructor(address implementation_){
+        implementation = implementation_;
+    }
+}
