502c72ae1124e2ac050d160e961f2214a2552c1d
465326302 <465326302@qq.com>
2023-03-15 16:00:22 +0800

    代理合约

--- exercise/46 代理合约.sol
+++ exercise/46 代理合约.sol
@@ -9,10 +9,15 @@ contract Proxy{
         implementation = implementation_;
     }
     fallback() external payable{
+        _delegate();
+    }
+    function _delegate() internal{
         address _implementation = implementation;
         assembly{
+            //calldatacopy(t, f, s)：将calldata（输入数据）从位置f开始复制s字节到mem（内存）的位置t
+            //calldatacopy是汇编指令 合约调用时把所有数据和参数存在calldata
             calldatacopy(0, 0, calldatasize())
-            //// delegatecall调用implementation
+            // delegatecall调用implementation
             let result := delegatecall(gas(), _implementation, 0, calldatasize(), 0, 0)
             returndatacopy(0, 0, returndatasize())
             switch result
@@ -21,10 +26,35 @@ contract Proxy{
                 }
             default {
                 return(0, returndatasize())
+            }
 
         }
     }
 
+}
+
+contract Logic {
+    address public implementation; // 与Proxy保持一致，防止插槽冲突
+    uint public x = 99; 
+    event CallSuccess(); // 调用成功事件
+
+    // 这个函数会释放CallSuccess事件并返回一个uint。
+    // 函数selector: 0xd09de08a
+    function increment() external returns(uint) {
+        emit CallSuccess();
+        return x + 1;
+    }
+}
+
+contract Caller{
+    address public proxy;
+    constructor(address _proxy){
+        proxy = _proxy;
+    }
+    function increment() external returns(uint){
+        ( , bytes memory data) = proxy.call(abi.encodeWithSignature("increment()"));
+        return abi.decode(data,(uint));
+    }
 }
 contract proxy{
     address public delegate;
