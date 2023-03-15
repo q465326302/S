57b6a2cb78ce3562a8f272ebb1b11e883effaf28
465326302 <465326302@qq.com>
2023-03-15 18:04:02 +0800

    结束

---
+++ exercise/47可升级合.sol
@@ -0,0 +1,39 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.0;
+
+contract SimpleUgrade{
+    address public implementation;
+    address public admin;
+    string public words;
+    constructor(address _implementation){
+        admin = msg.sender;
+        implementation = _implementation;
+
+    }
+    fallback() external payable{
+        (bool success,bytes memory data) = implementation.delegatecall(msg.data);
+    }
+    function upgrade(address newImplemention) external{
+        require(msg.sender == admin);
+        implementation = newImplemention;
+    }
+}
+
+contract Logic1{
+    address public implementation;
+    address public admin;
+    string public words;
+
+    function foo() public{
+        words = "old";
+    }
+}
+
+contract Logic2{
+    address public implementation;
+    address public admin;
+    string public words;
+    function foo() public{
+        words = "new";
+    }
+}
